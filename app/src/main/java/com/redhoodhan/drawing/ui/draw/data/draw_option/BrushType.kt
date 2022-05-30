7e0d53e20b9b8cf5b7ba48cd02ad2771a63d1ca0
RedhoodhanWnd <Donghanx@usc.edu>
2022-05-29 19:05:26 -0700

    Delete cache files

--- app/src/main/java/com/redhoodhan/drawing/ui/draw/data/draw_option/BrushType.kt
+++
@@ -1,8 +0,0 @@
-package com.redhoodhan.drawing.ui.draw.data.draw_option
-
-
-enum class BrushType {
-    // Note that the NORMAL brush type includes "Eraser", "Solid" and "Dashed"
-    NORMAL,
-    IMAGE
-}
--- app/src/main/java/com/redhoodhan/drawing/ui/draw/data/draw_option/PaintOption.kt
+++
@@ -1,6 +0,0 @@
-package com.redhoodhan.drawing.ui.draw.data.draw_option
-
-import android.graphics.Color
-import android.graphics.Paint
-
-data class PaintOption(val paint: Paint, var imageRes: Int? = 0, var strokeWidthBias: Float = 0f)
